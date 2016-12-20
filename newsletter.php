4f4dc0ef41a9e374618a186cddf4f9c9703491c3
Yael Vaisbuch <yael.vaisbuch@gmail.com>
2016-12-20 16:32:28 -0300

    new php newsletter by eduardo

--- newsletter.php
+++ newsletter.php
@@ -1,18 +1,21 @@
 <?php
 
 // get posted data into local variables
-$EmailFrom = Trim(stripslashes($_POST['EmailFrom'])); 
+//$EmailFrom = Trim(stripslashes($_POST['EmailFrom'])); 
 $EmailTo = "yael@trending.cl";
 $Subject = "Inscripción a Newsletter desde web TCL";
- 
+$email ="Newsletter";
 
 // validation
 $validationOK=true;
-if (Trim($EmailFrom)=="") $validationOK=false;
-if (!$validationOK) {
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
-  exit;
-}
+// if (Trim($EmailFrom)=="") $validationOK=false;
+// if (!$validationOK) {
+//   echo '<script language="javascript">alert("Lo sentimos, porfavor ingrese un email valido");</script>';
+//   exit;
+// }
+
+// campos nombres pymes
+$EmailFrom = Trim(stripslashes($_POST['EmailFrom'])); 
 
 // prepare email body text
 $Body = "";
@@ -21,13 +24,25 @@ $Body .= $EmailFrom;
 $Body .= "\n";
 
 // send email 
-$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+$success = mail($EmailTo, $Subject, $Body, "From:". $email);
 
 // redirect to success page 
 if ($success){
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=ok.htm\">";
+    ?>
+    <script language = 'javascript'>
+    alert('Gracias, hemos recibido satisfactoriamente su email.');
+	history.go(-1);
+	</script>
+	<?
+//  print "<meta http-equiv=\"refresh\" content=\"0;URL=ok.htm\">";
 }
 else{
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+?>
+<script language = 'javascript'>
+    alert('Algo a salido mal, porfavor intentelo nuevamente.');
+    history.go(-1);
+</script>
+<?
+//  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
 }
 ?>
